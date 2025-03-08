adb640f39a9b83b32b78d2fb8abecd580cd61c97
UpnitSingh <166071044+UpnitSingh@users.noreply.github.com>
2025-03-08 16:03:23 +0530

    Update ParllelMatrixMultiplication.cpp

--- ParllelMatrixMultiplication.cpp
+++ ParllelMatrixMultiplication.cpp
@@ -1,30 +1,27 @@
 #include <iostream>
-#include <vector>
-#include <cstdlib>
-#include <ctime>
-#include <fstream>
-#include <chrono>
 #include <pthread.h>
+#include <cstdlib>
+#include <chrono>  // For measuring execution time
 
-using namespace std;
-using namespace std::chrono;
+#define N 500  // Matrix size (Can be modified)
+#define NUM_THREADS 4  // Number of threads
 
-#define N 500  
-#define NUM_THREADS 4  // Change this to test different thread counts
+int A[N][N], B[N][N], C[N][N];  // Matrices
 
-vector<vector<int>> A, B, C;
-pthread_t threads[NUM_THREADS];
-
+// Structure for passing data to threads
 struct ThreadData {
     int start, end;
 };
 
+// Function to perform matrix multiplication for assigned rows
 void* multiplyPart(void* arg) {
     ThreadData* data = (ThreadData*)arg;
-    int start = data->start, end = data->end;
+    int start = data->start;
+    int end = data->end;
 
     for (int i = start; i < end; i++) {
         for (int j = 0; j < N; j++) {
+            C[i][j] = 0;
             for (int k = 0; k < N; k++) {
                 C[i][j] += A[i][k] * B[k][j];
             }
@@ -33,39 +30,48 @@ void* multiplyPart(void* arg) {
     pthread_exit(NULL);
 }
 
+// Function to execute parallel matrix multiplication
 void parallelMatrixMultiplication() {
+    pthread_t threads[NUM_THREADS];
     ThreadData threadData[NUM_THREADS];
     int rowsPerThread = N / NUM_THREADS;
-    
+
     for (int i = 0; i < NUM_THREADS; i++) {
         threadData[i].start = i * rowsPerThread;
         threadData[i].end = (i == NUM_THREADS - 1) ? N : (i + 1) * rowsPerThread;
         pthread_create(&threads[i], NULL, multiplyPart, (void*)&threadData[i]);
     }
-    
-    for (int i = 0; i < NUM_THREADS; i++)
+
+    for (int i = 0; i < NUM_THREADS; i++) {
         pthread_join(threads[i], NULL);
+    }
+}
+
+// Function to generate random matrices
+void generateRandomMatrix(int matrix[N][N]) {
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            matrix[i][j] = rand() % 10;  // Values between 0-9
+        }
+    }
 }
 
 int main() {
-    srand(time(0));
+    // Generate random matrices
+    generateRandomMatrix(A);
+    generateRandomMatrix(B);
 
-    A = vector<vector<int>>(N, vector<int>(N));
-    B = vector<vector<int>>(N, vector<int>(N));
-    C = vector<vector<int>>(N, vector<int>(N, 0));
+    // Measure execution time
+    auto start = std::chrono::high_resolution_clock::now();
 
-    for (int i = 0; i < N; i++)
-        for (int j = 0; j < N; j++)
-            A[i][j] = B[i][j] = rand() % 10;
-
-    auto start = high_resolution_clock::now();
+    // Perform parallel matrix multiplication
     parallelMatrixMultiplication();
-    auto end = high_resolution_clock::now();
 
-    auto duration = duration_cast<milliseconds>(end - start);
-    cout << "Pthreads Execution Time with " << NUM_THREADS << " threads: " << duration.count() << " ms" << endl;
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> duration = end - start;
 
-    writeMatrixToFile(C, "output_pthreads.txt");
+    // Print execution time only
+    std::cout << "Execution Time: " << duration.count() << " seconds" << std::endl;
 
     return 0;
 }
